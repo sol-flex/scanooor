c17abbad52a5b8f86bbfffbbb85da606026b6239
Marko <marko.pance@magiceden.io>
2023-02-07 01:31:31 +0700

    added mip1

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new Solana.PublicKey(
 const connection = new Solana.Connection(Solana.clusterApiUrl("mainnet-beta"));
 const metaplex = new Metaplex.Metaplex(connection);
 
+const TOKEN_METADATA_PROGRAM_ID = new Solana.PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s")
+
 const mintAddress = new Solana.PublicKey("SEED4sAHMmLKwiwndkPPCyGcY53i9RMoPagzXbHtpyK");
+//OCP
 const ocpProgramId = new Solana.PublicKey("ocp4vWUzA2z2XMYJ3QhM9vWdyoyoQwAFJhRdVTbvo9E");
+//Cardinal Labs
+const MINT_MANAGER_PROGRAM_ID = new Solana.PublicKey("mgr99QFMYByTqGPWmNqunV7vBLmWWXdSrHUfV8Jf3JM")
 const tokenAddress = new Solana.PublicKey("5hnVnNPS7GYbtZArsU9brYwVpf33b2NEhFyLP7CRRuMm")
 const walletAddress = new Solana.PublicKey("EyByisrhqy9uzWt7YYawnY89ZeQARN2DAfHCNSBMVESu")
 const associatedTokenAccount = new Solana.PublicKey("3GZsxfQLn55qkHUwFzBuwBPG865yTnxEM1MFCfeDqTYB")
@@ -23,7 +28,7 @@ async function findByMint(mintAddress) {
 
     const nft = await metaplex.nfts().findByMint({ mintAddress });
 
-    console.log(nft);
+    console.log("NFT RIGHT HERE: ", nft);
     console.log("supply: ", BigInt(nft.mint.supply.basisPoints));
     
 
@@ -73,18 +78,43 @@ async function findByMint(mintAddress) {
 }
 
 const findMintStatePk = (mint) => {
-    return  Solana.PublicKey.findProgramAddressSync(
+
+    const mintStatePk = Solana.PublicKey.findProgramAddressSync(
       [Anchor.utils.bytes.utf8.encode("mint_state"), mint.toBuffer()],
       ocpProgramId
     )[0];
 
+    console.log(mintStatePk)
+
     return mintStatePk;
+
 };
+
+const findTokenRecordAccount = (mint, ATA) => {
+
+    const mintStatePk = Solana.PublicKey.findProgramAddressSync(
+      [Anchor.utils.bytes.utf8.encode("metadata"), TOKEN_METADATA_PROGRAM_ID.toBuffer(), mint.toBuffer(), Anchor.utils.bytes.utf8.encode("token_record"), ATA.toBuffer()],
+      TOKEN_METADATA_PROGRAM_ID
+    )[0];
+
+    console.log(mintStatePk)
+
+    return mintStatePk;
+
+};
+
+// findTokenRecordAccount(new Solana.PublicKey("5KBbEfkCQQ3qwaRKJi1pjUVE2oGwASTrWco8Tw7WTeX8"), new Solana.PublicKey("8MFnE9KLFZJY9E59JSkN8vGT8Si28h73AhshqyPH1v9B"))
 
 const getMintStateAccountInfo = async (mintStatePk) => {
       const mintStateAcc =  await connection.getAccountInfo(mintStatePk);
       console.log(mintStateAcc);
       return mintStateAcc;
+}
+
+const getMintManagerAccountInfo = async (mintManagerPk) => {
+    const mintManagerAcc =  await connection.getAccountInfo(mintManagerPk);
+    console.log(mintManagerAcc);
+    return mintManagerAcc;
 }
 
 
@@ -111,12 +141,13 @@ async function getAccountInfo(pubKey) {
           }
       }) 
 
-      console.log(response.data.result.value.data.parsed);
+      console.log(response.data.result);
       //console.log("Token delegate: ", response.data.result.value.data.parsed.info.delegate)
       //console.log("Delegated amount: ", response.data.result.value.data.parsed.info.delegatedAmount.amount)
       // console.log("State of token account: ", response.data.result.value.data.parsed.info.state)
 
-      return response.data.result.value.data.parsed
+
+      return response.data.result
 
 }
 
@@ -152,6 +183,24 @@ async function findAssociatedTokenAddress(ownerWallet, tokenMint) {
 
 }
 
+async function findCardinalLabsMintManagerPDA(tokenMint) {
+
+    const mintManagerPDA = (await Solana.PublicKey.findProgramAddress(
+        [
+            Buffer.from('mint-manager'),
+            tokenMint.toBuffer(),
+        ],
+        MINT_MANAGER_PROGRAM_ID
+    ))[0];
+
+    console.log(mintManagerPDA)
+
+    return mintManagerPDA;
+
+}
+
+// findCardinalLabsMintManagerPDA(new Solana.PublicKey("7FaDrcUPSU3n2oKSmHzGsVNuWT9gqPbMa7SNfAAMWYNb"))
+
 async function getTokenLargestAccounts(mintAddress) {
 
     const connection = new Solana.Connection("https://burned-yolo-night.solana-mainnet.quiknode.pro/895b449661dccbdfbaf12ddef781ae2ac98f800b/");
@@ -164,16 +213,18 @@ async function getTokenLargestAccounts(mintAddress) {
     return new Solana.PublicKey(largestAccountInfo.value.data.parsed.info.owner);
 }
 
-getOwnerWalletFromMint(mintAddress);
+// getOwnerWalletFromMint(mintAddress);
 
-// findAssociatedTokenAddress(walletAddress, mintAddress)
+// findAssociatedTokenAddress(new Solana.PublicKey("1BWutmTvYPwDtmw9abTkS4Ssr8no61spGAvW1X6NDix"), new Solana.PublicKey("5KBbEfkCQQ3qwaRKJi1pjUVE2oGwASTrWco8Tw7WTeX8"))
 
-// getAccountInfo(new Solana.PublicKey("GNCetKuQbiH9z97UHainahJSwKJjWsL3we2hhJEEyj4j"));
+// getAccountInfo(new Solana.PublicKey("7evQhBswiztNd6HLvNWsh1Ekc3fmyvQGnL82uDepSMbw"));
 
-// findByMint(mintAddress);
+findByMint(new Solana.PublicKey("24hdE64cBuzWJq8KSGgKvgSBJz3TpE2Mt8AfC7NCf2Kk"));
 
-// getAccountInfo(new Solana.PublicKey("FjwKuHn91bYkB6f6YkPKnZWNwcaCFqEnoyyYdKP3C4Py"))
+// getAccountInfo(new Solana.PublicKey("AZ2HiSsD7G1gEt9stCV2ZdJprsLUKMTacwkj67YjG52"))
 
 // findAssociatedTokenAddress(walletAddress, mintAddress);
 
 // getTokenLargestAccounts(new Solana.PublicKey("2ZkGVeppRLQuG6GpBLLfsQiwJna9p1jzhpF8Wr3r9MRQ"))
+
+// findMintStatePk(new Solana.PublicKey("49CHrm1Vu9z3tvGGAXAxeBni8f7gwsui2kUHHJrMeipc"))
